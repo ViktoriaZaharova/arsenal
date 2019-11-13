624c1ebd6652cb53ab8288303736a3a94d907036
ViktoriaZaharova <zaharova181114@gmail.com>
2019-11-13 13:15:49 +0200

    add style

--- app/js/common.js
+++ app/js/common.js
@@ -3,4 +3,16 @@ $(document).ready(function() {
        $('.gifts-box').removeClass('gifts-box__check');
        $(this).addClass('gifts-box__check');
     });
+
+    $('.mobile-menu .menu-item__dropdown').click(function () {
+        $(this).find('.dropdown-menu').slideToggle();
+    });
+
+    $('.btn-mobile-menu').click(function () {
+        $('.mobile-menu').toggleClass('open');
+    });
+
+    $('.btn-close').click(function () {
+        $('.mobile-menu').removeClass('open');
+    });
 });
