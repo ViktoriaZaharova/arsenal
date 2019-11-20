21b30ab20225e016020cefa92ee5a2b11c9d7644
ViktoriaZaharova <zaharova181114@gmail.com>
2019-11-20 09:01:58 +0200

    add style

--- app/js/common.js
+++ app/js/common.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+
     $('.gifts-box').click(function () {
         $('.gifts-box').removeClass('gifts-box__check');
         $(this).addClass('gifts-box__check');
@@ -18,10 +20,14 @@ $(document).ready(function () {
 
     var activeStep = 1;
 
-
     $('.calc-test__total .btn-next').click(function (e) {
         e.preventDefault();
         $('.calc-test__body-box').hide().eq(activeStep++).show();
+
+        if(activeStep == $('.calc-test__body-box').length) {
+            $(this).hide();
+            $('.calc-test__total button').show();
+        }
 
         $('.readiness-scale__line .bg-line').css("width", "+=25%");
 
@@ -36,4 +42,8 @@ $(document).ready(function () {
         $('.readiness-scale').find('.color-blue').text(parseFloat($('.readiness-scale .color-blue').text()) - 25);
 
     });
+
+    $('[data-fancybox="images"]').fancybox();
+    $('[data-fancybox="gallery"]').fancybox();
+
 });
